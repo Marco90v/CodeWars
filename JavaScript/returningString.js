c7e68b59b52423cc4de7177dfecb36493f6cf000
Marco Velasquez Figarella <marcovelasquez90@hotmail.com>
2022-04-17 10:24:41 -0400

    retorna cadena (saludo)

---
+++ JavaScript/returningString.js
@@ -0,0 +1,17 @@
+/**
+ * Make a function that will return a greeting statement that uses an input; your program should return, 
+ * "Hello, <name> how are you doing today?". 
+ * [Make sure you type the exact thing I wrote or the program may not execute properly]
+ * ESPAÑOL
+ * Cree una función que devuelva una declaración de saludo que use una entrada; 
+ * su programa debería devolver, 
+ * "Hola, <nombre>, ¿cómo te va hoy?". 
+ * [Asegúrese de escribir exactamente lo que escribí o es posible que el programa no se ejecute correctamente]
+ */
+
+ function greet(name){
+    return `Hello, ${name} how are you doing today?`
+}
+
+console.log( greet("Marco") );
+console.log( greet("Zoimar") );
